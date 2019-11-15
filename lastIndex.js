636d461f5d9153d63a183142b1bb3960fe193679
qsaleh <qmsaleh@gmail.com>
2019-11-15 18:05:04 +0000

    function that outputs the last index value that a number is included in an array. If number is not included, output is -1

---
+++ lastIndex.js
@@ -0,0 +1,15 @@
+function lastIndexOf(array, number){
+  let output = -1;
+  for (let i = 0 ; i < array.length ; i++){
+    if (array[i] === number){
+      output = i;
+    }
+  }
+  return output;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
